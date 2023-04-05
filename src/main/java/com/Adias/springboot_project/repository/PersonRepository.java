ecd5257b29dce3ead43a890f4292436d5b68005f
macbook <jnrfotio@gmail.com>
2023-04-05 18:42:20 +0200

    query by example query by methodo name query by native

--- src/main/java/com/Adias/springboot_project/repository/PersonRepository.java
+++ src/main/java/com/Adias/springboot_project/repository/PersonRepository.java
@@ -11,11 +11,13 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     // query by example query by methodo name query by native
     @Query(value = "slect* from person where age >= 18", nativeQuery = true)
     Optional<List<Person>> findPersonMaggiorenni();
-    @Query(value = "select* from person where id = 2",nativeQuery = true)
+
+    @Query(value = "select* from person where id = 2", nativeQuery = true)
     Optional<Person> findWithId();
+
     List<Person> findByAgeGreaterThanEqual(Integer age);
 
-
-
-
 }
+
+
+
